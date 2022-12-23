ba46621b656886fa6fc18c740409d5eddb47d602
Amankr705 <ak.1000aman@gmail.com>
2022-12-23 15:59:04 +0530

    updated App
    fix: title
    added: react router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import Textform from './components/Textform';
-// import About from './components/About';
+import About from './components/About';
 import { useState } from 'react';
 import Alert from './components/Alert';
-// import { BrowserRouter, Routes, Route} from "react-router-dom";
+import { BrowserRouter, Routes, Route} from "react-router-dom";
 
 
 
@@ -31,12 +31,11 @@ function App() {
       setMode('dark');
       document.body.style.background = '#042743';
       showAlert("Dark mode has been enabled", "success");
-      document.title = 'TextUtils - Dark';
+      // document.title = 'TextUtils - Dark';
     }else{
       setMode('light');
       document.body.style.background = 'white';
       showAlert("Light mode has been enabled", "success");
-
     }
   }
   return (
@@ -44,20 +43,20 @@ function App() {
       {/* <Navbar/> */}
       {/* <Navbar title="TextUtils" aboutText="About textUtils" /> */}
 
-      {/* <BrowserRouter> */}
+      <BrowserRouter>
         <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
         <Alert alert={alert}/>
         <div className="container my-3">
-        {/* <Routes> */}
+        <Routes>
           {/* /users --> element 1
           /users/profile --> element 2 */}
 
-            {/* <Route exact path="/" element={<Textform showAlert={showAlert} heading="Enter the text to analyze" mode={mode} toggleMode={toggleMode}/>} />  */}
-            <Textform showAlert={showAlert} heading="Enter the text to analyze" mode={mode} toggleMode={toggleMode} />
-            {/* <Route exact path="/about" element={<About />} /> */}
-          {/* </Routes> */}
+            <Route exact path="/" element={<Textform showAlert={showAlert} heading="TextUtils - Word & Character Counter" mode={mode} toggleMode={toggleMode}/>} /> 
+            {/* <Textform showAlert={showAlert} heading="Enter the text to analyze" mode={mode} toggleMode={toggleMode} /> */}
+            <Route exact path="/about" element={<About mode={mode} />} />
+          </Routes>
         </div>
-      {/* </BrowserRouter> */}
+      </BrowserRouter>
     
     </>
 
