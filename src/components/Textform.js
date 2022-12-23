ddd74b9edad6689e6987edd4aca0f1feb435e5fd
Amankr705 <ak.1000aman@gmail.com>
2022-12-23 16:24:19 +0530

    Fix: count words & func handleCopy

--- src/components/Textform.js
+++ src/components/Textform.js
@@ -30,9 +30,10 @@ export default function Textform(props) {
     }
 
     const handleCopy = ()=> {
-        var text = document.getElementById("myBox");
-        <text className="Select"></text>
-        navigator.clipboard.writeText(text.value);
+        // var text = document.getElementById("myBox");
+        // <text className="Select"></text>
+        // navigator.clipboard.writeText(text.value);
+        navigator.clipboard.writeText(text);
         props.showAlert("Copied to the clipboard! ", "success");
     }
         
@@ -54,7 +55,7 @@ export default function Textform(props) {
         </div>
         <div className="container my-2" style={{color: props.mode==='light'?'#042743':'white'}}>
             <h2>Your text summary</h2>
-            <p>{text.split(" ").filter((element) => {return element.length!==0} ).length} words and {text.length} characters and {text.split(".").length} sentences</p>
+            <p>{text.split(/\s+/).filter((element) => {return element.length!==0} ).length} words and {text.length} characters and {text.split(".").length} sentences</p>
             <p>{0.008 * text.split(" ").filter((element) => { return element.length!==0}).length} Minutes read</p>
             <h2>Preview</h2>
             <p>{text.length>0?text:"Nothing to preview!"} </p>
